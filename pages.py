fcd4e2bb67900e5bdfe943137fff5f2edc4836cd
Devin Sova <devin.m.sova@gmail.com>
2020-02-20 00:04:11 -0800

    Added P4AU links and setup linter

---
+++ pages.py
@@ -0,0 +1,26 @@
+P4AU = {
+    "Aigis": "https://www.dustloop.com/wiki/index.php?title=P4AU/Aigis",
+    "Akihiko_Sanada": "https://www.dustloop.com/wiki/index.php?title=P4AU/Akihiko_Sanada",
+    "Chie_Satonaka": "https://www.dustloop.com/wiki/index.php?title=P4AU/Chie_Satonaka",
+    "Elizabeth": "https://www.dustloop.com/wiki/index.php?title=P4AU/Elizabeth",
+    "Junpei_Iori": "https://www.dustloop.com/wiki/index.php?title=P4AU/Junpei_Iori",
+    "Kanji Tatsumi": "https://www.dustloop.com/wiki/index.php?title=P4AU/Kanji_Tatsumi",
+    "Ken_Amada": "https://www.dustloop.com/wiki/index.php?title=P4AU/Ken_Amada",
+    "Labrys": "https://www.dustloop.com/wiki/index.php?title=P4AU/Labrys",
+    "Margaret": "https://www.dustloop.com/wiki/index.php?title=P4AU/Margaret",
+    "Marie": "https://www.dustloop.com/wiki/index.php?title=P4AU/Marie",
+    "Sho_MINAZUKI": "https://www.dustloop.com/wiki/index.php?title=P4AU/Sho_MINAZUKI",
+    "Mitsuru_Kirijo": "https://www.dustloop.com/wiki/index.php?title=P4AU/Mitsuru_Kirijo",
+    "Naoto_Shirogane": "https://www.dustloop.com/wiki/index.php?title=P4AU/Naoto_Shirogane",
+    "Rise_Kujikawa": "https://www.dustloop.com/wiki/index.php?title=P4AU/Rise_Kujikawa",
+    "Shadow_Labrys": "https://www.dustloop.com/wiki/index.php?title=P4AU/Shadow_Labrys",
+    "SHO_Minazuki": "https://www.dustloop.com/wiki/index.php?title=P4AU/SHO_Minazuki",
+    "Teddie": "https://www.dustloop.com/wiki/index.php?title=P4AU/Teddie",
+    "Tohru_Adachi": "https://www.dustloop.com/wiki/index.php?title=P4AU/Tohru_Adachi",
+    "Yosuke_Hanamura": "https://www.dustloop.com/wiki/index.php?title=P4AU/Yosuke_Hanamura",
+    "Yu_Narukami": "https://www.dustloop.com/wiki/index.php?title=P4AU/Yu_Narukami",
+    "Yukari_Takeba": "https://www.dustloop.com/wiki/index.php?title=P4AU/Yukari_Takeba",
+    "Yukiko_Amagi": "https://www.dustloop.com/wiki/index.php?title=P4AU/Yukiko_Amagi"
+}
+
+print(P4AU)
